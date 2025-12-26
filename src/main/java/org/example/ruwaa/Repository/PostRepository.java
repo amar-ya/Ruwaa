07a7fae79d41b992bc2800cef717c76505b32810
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-12-27 00:30:27 +0300

    open AI api

--- src/main/java/org/example/ruwaa/Repository/PostRepository.java
+++ src/main/java/org/example/ruwaa/Repository/PostRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer>
 {
     @Query("select m from Post m where m.id =:id")
-    Optional<Post> findMediaById(Integer id);
+    Optional<Post> findPostById(Integer id);
+
+    List<Post> findPostByUsers_id(Integer user_id);
 }
--- src/main/java/org/example/ruwaa/Service/PostService.java
+++ src/main/java/org/example/ruwaa/Service/PostService.java
@@ -9,6 +9,7 @@ import org.example.ruwaa.Repository.UsersRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -33,7 +34,7 @@ public class PostService
     }
 
     public void updatePost(Integer id, Post post){
-        Post m = postRepository.findMediaById(id).orElseThrow(() -> new ApiException("post not found"));
+        Post m = postRepository.findPostById(id).orElseThrow(() -> new ApiException("post not found"));
 
         m.setTitle(post.getTitle());
         m.setContent(post.getContent());
@@ -41,8 +42,41 @@ public class PostService
     }
 
     public void deletePost(Integer id){
-        Post m = postRepository.findMediaById(id).orElseThrow(() -> new ApiException("post not found"));
+        Post m = postRepository.findPostById(id).orElseThrow(() -> new ApiException("post not found"));
 
         postRepository.delete(m);
+    }
+
+    //1) 24
+    public Post viewPost(Integer postId){
+        Post p = postRepository.findPostById(postId).orElseThrow(() -> new ApiException("post not found"));
+
+    p.setViews(p.getViews()+1);
+    postRepository.save(p);
+    return p;
+    }
+    //1)  1
+    public List<Post> getMyPost(Integer userId){
+        Users user = usersRepository.findUserById(userId).orElseThrow(() -> new ApiException("Account not found"));
+        return postRepository.findPostByUsers_id(userId);
+    }
+
+
+    public String reviewMyWork(Integer postId){
+        Post p = postRepository.findPostById(postId).orElseThrow(() -> new ApiException("post not found"));
+        if(!p.getType().equals("public_work")&&!p.getType().equals("private_work")) throw new ApiException("this is not work post");
+
+        //AI
+        return "";
+    }
+    //1)  6
+    public void changeVisibilityToPublic(Integer postId){
+        Post p = postRepository.findPostById(postId).orElseThrow(() -> new ApiException("post not found"));
+        if(!p.getType().equals("public_work")&&!p.getType().equals("private_work")) throw new ApiException("this is not work post");
+        if(p.getType().equals("public_work")) throw new ApiException("this work is already Public");
+
+        p.setType("public_work");
+        postRepository.save(p);
+
     }
 }
